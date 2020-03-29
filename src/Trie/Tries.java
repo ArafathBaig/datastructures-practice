7c4a7336402df6bfd57a93549746431302e706dc
Arafat Najjashi <arafath.moghul@gmail.com>
2020-03-29 20:11:02 +0530

    Tries commit

---
+++ src/Trie/Tries.java
@@ -0,0 +1,87 @@
+package Trie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tries {
+    public static void main(String[] args) {
+        final List<String> setOfStrings = new ArrayList<>();
+        setOfStrings.add("pqrs");
+        setOfStrings.add("pprt");
+        setOfStrings.add("psst");
+        setOfStrings.add("qqrs");
+        setOfStrings.add("pqrs");
+
+        Trie trie = new Trie();
+        setOfStrings.forEach(trie::insert);
+        System.out.println(trie.query("pqrs"));
+        System.out.println(trie.query("abc"));
+        System.out.println(trie.query("psst"));
+        System.out.println(trie.query("psst"));
+        trie.update("qqrs","psst");
+        
+
+    }
+}
+
+class Trie{
+    TrieNode root;
+
+    public Trie(){
+        this.root = new TrieNode();
+    }
+
+    public int query(String s) {
+        TrieNode current = root;
+        for (int i = 0; i < s.length(); i++) {
+            if(current == null){
+                return 0;
+            }
+            current =  current.next(s.charAt(i));
+        }
+        return current.terminating;
+    }
+
+    public void insert(String s){
+        TrieNode current = root;
+        for(int i = 0; i < s.length();i++){
+            if(current.trieNodes[s.charAt(i)- 'a'] == null){
+                current.trieNodes[s.charAt(i) - 'a'] = new TrieNode();
+            }
+
+            current = current.next(s.charAt(i));
+        }
+        current.terminating++;
+    }
+
+    public void delete(String s){
+        TrieNode current = root;
+        for (int i = 0; i < s.length(); i++) {
+            if(current == null){
+                return;
+            }
+            current =  current.next(s.charAt(i));
+        }
+        if(current.terminating != 0)
+            current.terminating--;
+        else
+            throw new RuntimeException("String does not exist");
+    }
+
+    public void update(final String absolete, final String updated){
+        delete(absolete);
+        insert(updated);
+    }
+}
+
+class TrieNode {
+
+    int terminating;
+     TrieNode[] trieNodes = new TrieNode[26];
+
+    public TrieNode next(char c){
+        return trieNodes[c-'a'];
+    }
+
+
+}
